519852294080e6b080827653ef6bf7aaabe02595
Kevin <kevin.breneman@zipscene.com>
2017-08-18 13:59:10 -0400

    added the rest of the result hand evaluators

--- test/hand-eval.js
+++ test/hand-eval.js
@@ -106,6 +106,138 @@ describe('handEval', function() {
 			);
 		});
 
+		it('should detect a flush', function() {
+			checkHandResult(
+				getHandResult(makeHand([ 'Kc', '8c', 'As', '6c', 'Qc', '4c', '2c' ])),
+				{
+					type: 'flush',
+					suit: getSuitFromString('c'),
+					kickerValues: [
+						getValueFromString('K'),
+						getValueFromString('Q'),
+						getValueFromString('8'),
+						getValueFromString('6'),
+						getValueFromString('4')
+					],
+					hand: [ 'Kc', 'Qc', '8c', '6c', '4c' ]
+				}
+			);
+			expect(
+				getHandResult(makeHand([ 'Tc', '8h', '7c', '6c', '5c', '4c', '3c' ])).type
+			).to.not.equal('flush');
+		});
+
+		it('should detect a straight', function() {
+			checkHandResult(
+				getHandResult(makeHand([ 'Ac', 'Kc', 'Qd', '4s', 'Ts', 'Jh' ])),
+				{
+					type: 'straight',
+					highValue: getValueFromString('A'),
+					hand: [ 'Ac', 'Kc', 'Qd', 'Jh', 'Ts' ]
+				}
+			);
+			checkHandResult(
+				getHandResult(makeHand([ 'Ad', '4s', 'As', '2h', 'Kh', '3c', '5c' ])),
+				{
+					type: 'straight',
+					highValue: getValueFromString('5'),
+					hand: [ '5c', '4s', '3c', '2h', 'As' ]
+				}
+			);
+			checkHandResult(
+				getHandResult(makeHand([ '3s', '4s', '5h', '6h', '7c', '8h', '9s' ])),
+				{
+					type: 'straight',
+					highValue: getValueFromString('9'),
+					hand: [ '9s', '8h', '7c', '6h', '5h' ]
+				}
+			);
+			expect(
+				getHandResult(makeHand([ 'Ks', 'Qs', 'Jh', 'Td', '9s', '6s', '3s' ])).type
+			).to.not.equal('straight');
+		});
+
+		it('should detect a three-of-a-kind', function() {
+			checkHandResult(
+				getHandResult(makeHand([ '4c', '5h', '5s', '6c', '9h', '5d', 'Kc' ])),
+				{
+					type: 'three-of-a-kind',
+					value: getValueFromString('5'),
+					kickerValues: [ getValueFromString('K'), getValueFromString('9') ],
+					hand: [ '5h', '5s', '5d', 'Kc', '9h' ]
+				}
+			);
+			expect(
+				getHandResult(makeHand([ 'Ks', 'Qs', 'Kd', 'Qd', '5c', 'Kh' ])).type
+			).to.not.equal('three-of-a-kind');
+			expect(
+				getHandResult(makeHand([ 'Ks', 'Qs', 'Kd', 'Kh', 'Kc', '2c', '4d' ])).type
+			).to.not.equal('three-of-a-kind');
+			expect(
+				getHandResult(makeHand([ 'Ks', 'Qs', 'Kd', 'Kh', 'Jc', 'Th', '9d' ])).type
+			).to.not.equal('three-of-a-kind');
+		});
+
+		it('should detect a two-pair', function() {
+			checkHandResult(
+				getHandResult(makeHand([ '4c', '5h', '5s', '6c', '4h', 'As', 'Kc' ])),
+				{
+					type: 'two-pair',
+					values: [ getValueFromString('5'), getValueFromString('4') ],
+					kickerValues: [ getValueFromString('A') ],
+					hand: [ '5h', '5s', '4h', '4c', 'As' ]
+				}
+			);
+			checkHandResult(
+				getHandResult(makeHand([ '4c', '5h', '5s', '2c', '4h', 'As', 'Ac' ])),
+				{
+					type: 'two-pair',
+					values: [ getValueFromString('A'), getValueFromString('5') ],
+					kickerValues: [ getValueFromString('4') ],
+					hand: [ 'As', 'Ac', '5s', '5h', '4h' ]
+				}
+			);
+			expect(
+				getHandResult(makeHand([ 'Ks', 'Qs', 'Kd', 'Qd', 'Kh' ])).type
+			).to.not.equal('two-pair');
+		});
+
+		it('should detect a pair', function() {
+			checkHandResult(
+				getHandResult(makeHand([ '4c', '5h', '5s', '6c', 'Th', 'Ad', 'Kc' ])),
+				{
+					type: 'pair',
+					value: getValueFromString('5'),
+					kickerValues: [
+						getValueFromString('A'),
+						getValueFromString('K'),
+						getValueFromString('T')
+					],
+					hand: [ '5h', '5s', 'Ad', 'Kc', 'Th' ]
+				}
+			);
+			expect(
+				getHandResult(makeHand([ 'Ks', 'Qs', 'Kd', 'Qd', '5c', 'Jh' ])).type
+			).to.not.equal('pair');
+		});
+
+		it('should calculate a high-cards result if no other result is present', function() {
+			checkHandResult(
+				getHandResult(makeHand([ '4c', '5h', 'Qs', '6c', 'Th', 'Ad', 'Jc' ])),
+				{
+					type: 'high-cards',
+					kickerValues: [
+						getValueFromString('A'),
+						getValueFromString('Q'),
+						getValueFromString('J'),
+						getValueFromString('T'),
+						getValueFromString('6')
+					],
+					hand: [ 'Ad', 'Qs', 'Jc', 'Th', '6c' ]
+				}
+			);
+		});
+
 	});
 
 });
